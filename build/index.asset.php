137c6ca536e05f9e410f83312eb250aa0695995c
Paolo Tesei <ptesei@beapi.fr>
2026-09-25 14:12:42 +0000

    Release version 0.2.2

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-rich-text', 'wp-style-engine'), 'version' => '313edb99bb657513ca4b');
